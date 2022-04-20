61665defb31c93e69b727a43660cb9b68c3b5251
shashi <shashikumar3089@gmail.com>
2022-04-21 00:04:13 +0530

    initial commit

---
+++ Onboarding.java
@@ -0,0 +1,13 @@
+public class Onboarding {
+	
+public static void main(String [] args) {
+
+	display();
+}	
+	
+private static void display() {
+String str1 = "Shashi";
+String str2 = "Shwetha";
+}
+	
+}
